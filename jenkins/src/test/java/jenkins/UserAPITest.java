4c31b839f5074b25687ac8137671324d7a406d28
Subash.S <141321290+SSubash24@users.noreply.github.com>
2025-05-19 21:46:08 +0530

    Update UserAPITest.java

--- jenkins/src/test/java/jenkins/UserAPITest.java
+++ jenkins/src/test/java/jenkins/UserAPITest.java
@@ -3,20 +3,31 @@ package jenkins;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class UserAPITest {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
 
     @Test
     public void getUserDetails() {
         Response response = RestAssured
                 .given()
-                .baseUri("https://reqres.in")
                 .when()
                 .get("/api/users/2");
 
+        // Print response for visibility
+        response.prettyPrint();
+
+        // Assert status code
         Assert.assertEquals(response.getStatusCode(), 200);
-        Assert.assertTrue(response.getBody().asString().contains("Janet"));
+
+        // Assert specific field
+        String firstName = response.jsonPath().getString("data.first_name");
+        Assert.assertEquals(firstName, "Janet");
     }
 }
-
